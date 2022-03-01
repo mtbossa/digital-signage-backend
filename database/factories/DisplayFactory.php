f294f784a4e2b60aab4dc8e5c8930d78f2676cb1
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-03-01 13:24:54 -0300

    update factory to use faker randomElement()

--- database/factories/DisplayFactory.php
+++ database/factories/DisplayFactory.php
@@ -19,16 +19,11 @@ class DisplayFactory extends Factory
   {
     return [
       'name' => $this->faker->text(50),
-      'size' => $this->screen_sizes[$this->_randomIndex($this->screen_sizes)],
-      'width' => $this->screen_widths[$this->_randomIndex($this->screen_widths)],
-      'height' => $this->screen_heights[$this->_randomIndex($this->screen_heights)],
+      'size' => $this->faker->randomElement($this->screen_sizes),
+      'width' => $this->faker->randomElement($this->screen_widths),
+      'height' => $this->faker->randomElement($this->screen_heights),
       'touch' => $this->faker->boolean(20),
       'observation' => $this->faker->boolean ? $this->faker->text(50) : null,
     ];
-  }
-
-  private function _randomIndex(array $array): int
-  {
-    return mt_rand(0, (count($array) - 1));
   }
 }
