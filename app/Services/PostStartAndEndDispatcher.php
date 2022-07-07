2bb671581615f0d943de398d7572bd793c13f5e0
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-07-07 14:54:38 -0300

    Refactor variables

--- app/Services/PostStartAndEndDispatcher.php
+++ app/Services/PostStartAndEndDispatcher.php
@@ -10,37 +10,50 @@ use Carbon\Carbon;
 class PostStartAndEndDispatcher
 {
   private Post $post;
+  private Carbon $now;
+  private Carbon $startDate;
+  private Carbon $endDate;
+  private Carbon $startTime;
+  private Carbon $endTime;
+
+  public function __construct()
+  {
+    $this->now = Carbon::now();
+  }
 
   public function setPost(Post $post): PostStartAndEndDispatcher
   {
     $this->post = $post;
+    $this->setDatesAndTimes();
     return $this;
+  }
+
+  private function setDatesAndTimes()
+  {
+    $this->startDate = Carbon::createFromFormat('Y-m-d', $this->post->start_date);
+    $this->endDate = Carbon::createFromFormat('Y-m-d', $this->post->end_date);
+    $this->startTime = Carbon::createFromTimeString($this->post->start_time);
+    $this->endTime = Carbon::createFromTimeString($this->post->end_time);
   }
 
   public function run(): void
   {
-    $now = Carbon::now();
-    $startDate = Carbon::createFromFormat('Y-m-d', $this->post->start_date);
-    $endDate = Carbon::createFromFormat('Y-m-d', $this->post->end_date);
-    $startTime = Carbon::createFromTimeString($this->post->start_time);
-    $endTime = Carbon::createFromTimeString($this->post->end_time);
-
-    if ($startDate->startOfDay()->isAfter($now)) {
+    if ($this->startDate->startOfDay()->isAfter($this->now)) {
       foreach ($this->post->displays as $display) {
         StartPost::dispatch($this->post);
       }
     } else {
-      if ($startTime->isAfter($endTime) || ($startTime->isSameHour($endTime)) && $startTime->isAfter($endTime)) {
-        $startOfToday = $now->copy()->startOf('day');
-        $endOfToday = $now->copy()->endOf('day');
+      if ($this->startTime->isAfter($this->endTime) || ($this->startTime->isSameHour($this->endTime)) && $this->startTime->isAfter($this->endTime)) {
+        $startOfToday = $this->now->copy()->startOf('day');
+        $endOfToday = $this->now->copy()->endOf('day');
 
-        $endYesterdayMinutesShowToday = $startOfToday->diffInMinutes($endTime);
+        $endYesterdayMinutesShowToday = $startOfToday->diffInMinutes($this->endTime);
         $todayShowEndYesterday = $startOfToday->copy()->addMinute($endYesterdayMinutesShowToday);
 
-        $startTodayMinutesShowToday = $endOfToday->diffInMinutes($startTime);
+        $startTodayMinutesShowToday = $endOfToday->diffInMinutes($this->startTime);
         $todayShowStartToday = $endOfToday->copy()->subMinute($startTodayMinutesShowToday);
 
-        if ($now->isBetween($todayShowEndYesterday, $todayShowStartToday)) {
+        if ($this->now->isBetween($todayShowEndYesterday, $todayShowStartToday)) {
           foreach ($this->post->displays as $display) {
             StartPost::dispatch($this->post);
           }
@@ -50,7 +63,7 @@ class PostStartAndEndDispatcher
           }
         }
       } else {
-        if ($this->post->start_date <= now()->format('Y-m-d') && ($now->isAfter($startTime) && $now->isBefore($endTime))) {
+        if ($this->post->start_date <= now()->format('Y-m-d') && ($this->now->isAfter($this->startTime) && $this->now->isBefore($this->endTime))) {
           foreach ($this->post->displays as $display) {
             event(new PostStarted($this->post, $display));
           }
