477aa902301159a33dc2e145b429568584cd5f50
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-07-08 17:09:05 -0300

    Need to add Bus and Event fake because we're not controlling the now time in those tests

--- tests/Feature/Post/PostTest.php
+++ tests/Feature/Post/PostTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Post;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Str;
 use Tests\Feature\Post\Traits\PostTestsTrait;
 use Tests\Feature\Traits\AuthUserTrait;
@@ -10,60 +12,76 @@ use Tests\TestCase;
 
 class PostTest extends TestCase
 {
-  use RefreshDatabase, PostTestsTrait, AuthUserTrait;
+    use RefreshDatabase, PostTestsTrait, AuthUserTrait;
 
-  public function setUp(): void
-  {
-    parent::setUp();
+    public function setUp(): void
+    {
+        parent::setUp();
 
-    $this->_authUser();
-    $this->media = $this->_createMedia();
-    $this->recurrence = $this->_createRecurrence();
-    $this->post = $this->_createPost(['media_id' => $this->media->id, 'recurrence_id' => $this->recurrence->id]);
-  }
+        Bus::fake();
+        Event::fake();
 
-  /** @test */
-  public function create_post()
-  {
-    $post_data = $this->_makePost(['media_id' => $this->media->id], false)->toArray();
+        $this->_authUser();
+        $this->media = $this->_createMedia();
+        $this->recurrence = $this->_createRecurrence();
+        $this->post = $this->_createPost([
+            'media_id'      => $this->media->id,
+            'recurrence_id' => $this->recurrence->id
+        ]);
+    }
 
-    $response = $this->postJson(route('posts.store'), [...$post_data, 'displays_ids' => null]);
+    /** @test */
+    public function create_post()
+    {
+        $post_data = $this->_makePost(['media_id' => $this->media->id], false)
+            ->toArray();
 
-    $this->assertDatabaseHas('posts', $post_data);
+        $response = $this->postJson(route('posts.store'),
+            [...$post_data, 'displays_ids' => null]);
 
-    $response->assertCreated()->assertJson($post_data);
-  }
+        $this->assertDatabaseHas('posts', $post_data);
 
-  /** @test */
-  public function delete_post()
-  {
-    $response = $this->deleteJson(route('posts.destroy', $this->post->id));
-    $this->assertDatabaseMissing('posts', ['id' => $this->post->id]);
-    $response->assertOk();
-  }
+        $response->assertCreated()->assertJson($post_data);
+    }
 
-  /** @test */
-  public function fetch_single_post()
-  {
-    $this->getJson(route('posts.show',
-      $this->post->id))->assertOk()->assertJson($this->post->toArray());
-  }
+    /** @test */
+    public function delete_post()
+    {
+        $response = $this->deleteJson(route('posts.destroy', $this->post->id));
+        $this->assertDatabaseMissing('posts', ['id' => $this->post->id]);
+        $response->assertOk();
+    }
 
-  /** @test */
-  public function fetch_all_posts()
-  {
-    $this->_createPost(['media_id' => $this->media->id]);
+    /** @test */
+    public function fetch_single_post()
+    {
+        $this->getJson(route('posts.show',
+            $this->post->id))->assertOk()->assertJson($this->post->toArray());
+    }
 
-    $this->getJson(route('posts.index'))->assertOk()->assertJsonCount(2)->assertJsonFragment($this->post->toArray());
-  }
+    /** @test */
+    public function fetch_all_posts()
+    {
+        $this->_createPost(['media_id' => $this->media->id]);
 
-  /** @test */
-  public function ensure_only_description_is_updated_even_if_more_fields_are_sent()
-  {
-    $old_media_id = $this->post->media->id;
-    $new_values = ['description' => Str::random(20), 'media_id' => 2, 'recurrence_id' => 2];
+        $this->getJson(route('posts.index'))->assertOk()->assertJsonCount(2)
+            ->assertJsonFragment($this->post->toArray());
+    }
 
-    $this->putJson(route('posts.update', $this->post->id),
-      $new_values)->assertOk()->assertJson(['description' => $new_values['description'], 'media_id' => $old_media_id]);
-  }
+    /** @test */
+    public function ensure_only_description_is_updated_even_if_more_fields_are_sent(
+    )
+    {
+        $old_media_id = $this->post->media->id;
+        $new_values = [
+            'description'   => Str::random(20), 'media_id' => 2,
+            'recurrence_id' => 2
+        ];
+
+        $this->putJson(route('posts.update', $this->post->id),
+            $new_values)->assertOk()->assertJson([
+            'description' => $new_values['description'],
+            'media_id'    => $old_media_id
+        ]);
+    }
 }
