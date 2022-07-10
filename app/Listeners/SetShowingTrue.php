fe9a600caf80c85d6d66e2c2f55e11c859f0b1a4
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-07-10 09:19:22 -0300

    Clean code

--- app/Listeners/SetShowingTrue.php
+++ app/Listeners/SetShowingTrue.php
@@ -6,23 +6,6 @@ use App\Events\ShouldStartPost;
 
 class SetShowingTrue
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Handle the event.
-     *
-     * @param  object  $event
-     *
-     * @return void
-     */
     public function handle(ShouldStartPost $event): void
     {
         $event->post->showing = true;
