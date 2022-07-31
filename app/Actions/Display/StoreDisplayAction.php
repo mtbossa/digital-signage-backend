75bbf1fa52dd9fc72d3db8b6bf7f7936844719f5
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-07-31 14:08:43 -0300

    Store store with display

--- app/Actions/Display/StoreDisplayAction.php
+++ app/Actions/Display/StoreDisplayAction.php
@@ -5,18 +5,24 @@ namespace App\Actions\Display;
 use App\Http\Requests\Display\StoreDisplayRequest;
 use App\Models\Display;
 use App\Models\Raspberry;
+use App\Models\Store;
 
 class StoreDisplayAction
 {
-  public function handle(StoreDisplayRequest $request): Display
-  {
-    $display = Display::create($request->except(['raspberry_id']));
+    public function handle(StoreDisplayRequest $request): Display
+    {
+        $display = Display::create($request->except(['raspberry_id']));
 
-    if ($request->raspberry_id) {
-      $raspberry = Raspberry::findOrFail($request->raspberry_id);
-      $display->raspberry()->save($raspberry);
+        if ($request->raspberry_id) {
+            $raspberry = Raspberry::findOrFail($request->raspberry_id);
+            $display->raspberry()->save($raspberry);
+        }
+
+        if ($request->store_id) {
+            $store = Store::findOrFail($request->store_id);
+            $display->store()->associate($store);
+        }
+
+        return $display;
     }
-
-    return $display;
-  }
 }
