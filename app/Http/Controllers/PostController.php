ef12f75340ea83fecc837244994942d55d894073
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-05 23:06:22 -0300

    Add withDisplaydsIds param

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Actions\Post\StorePostAction;
 use App\Http\Requests\Post\StorePostRequest;
 use App\Http\Requests\Post\UpdatePostRequest;
+use App\Models\Display;
 use App\Models\Post;
 use App\Services\PostDispatcherService;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -19,25 +21,39 @@ class PostController extends Controller
 
   public function store(
     StorePostRequest $request,
-        StorePostAction $action,
-        PostDispatcherService $service
-    ): Post {
-        return $action->handle($request, $service);
-    }
+    StorePostAction $action,
+    PostDispatcherService $service
+  ): Post {
+    return $action->handle($request, $service);
+  }
 
-    public function show(Post $post): Post
-    {
-        return $post;
-    }
-
-    public function update(UpdatePostRequest $request, Post $post): Post
-    {
-        $post->update($request->validated());
-
-        if ($request->has('displays_ids')) {
-            $post->displays()->sync($request->displays_ids);
-            $post['displays'] = $post->displays->toArray();
+  public function show(Request $request, Post $post): Post
+  {
+    if ($request->withDisplaysIds) {
+      $post->load([
+        'displays' => function (BelongsToMany $query) {
+          $query->select('displays.id');
         }
+      ]);
+
+      $post->displays_ids = $post->displays->map(function (Display $test) {
+        return $test->id;
+      })->toArray();
+
+      unset($post->displays);
+    }
+
+    return $post;
+  }
+
+  public function update(UpdatePostRequest $request, Post $post): Post
+  {
+    $post->update($request->validated());
+
+    if ($request->has('displays_ids')) {
+      $post->displays()->sync($request->displays_ids);
+      $post['displays'] = $post->displays->toArray();
+    }
 
         return $post;
     }
--- tests/Feature/Post/PostTest.php
+++ tests/Feature/Post/PostTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Post;
 
+use App\Models\Display;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Event;
@@ -12,76 +13,86 @@ use Tests\TestCase;
 
 class PostTest extends TestCase
 {
-    use RefreshDatabase, PostTestsTrait, AuthUserTrait;
+  use RefreshDatabase, PostTestsTrait, AuthUserTrait;
 
-    public function setUp(): void
-    {
-        parent::setUp();
+  public function setUp(): void
+  {
+    parent::setUp();
 
-        Bus::fake();
-        Event::fake();
+    Bus::fake();
+    Event::fake();
 
-        $this->_authUser();
-        $this->media = $this->_createMedia();
-        $this->recurrence = $this->_createRecurrence();
-        $this->post = $this->_createPost([
-            'media_id'      => $this->media->id,
-            'recurrence_id' => $this->recurrence->id
-        ]);
-    }
+    $this->_authUser();
+    $this->media = $this->_createMedia();
+    $this->recurrence = $this->_createRecurrence();
+    $this->post = $this->_createPost([
+      'media_id' => $this->media->id,
+      'recurrence_id' => $this->recurrence->id
+    ]);
+  }
 
-    /** @test */
-    public function create_post()
-    {
-        $post_data = $this->_makePost(['media_id' => $this->media->id], false)
-            ->toArray();
+  /** @test */
+  public function create_post()
+  {
+    $post_data = $this->_makePost(['media_id' => $this->media->id], false)
+      ->toArray();
 
-        $response = $this->postJson(route('posts.store'),
-            [...$post_data, 'displays_ids' => null]);
+    $response = $this->postJson(route('posts.store'),
+      [...$post_data, 'displays_ids' => null]);
 
-        $this->assertDatabaseHas('posts', $post_data);
+    $this->assertDatabaseHas('posts', $post_data);
 
-        $response->assertCreated()->assertJson($post_data);
-    }
+    $response->assertCreated()->assertJson($post_data);
+  }
 
-    /** @test */
-    public function delete_post()
-    {
-        $response = $this->deleteJson(route('posts.destroy', $this->post->id));
-        $this->assertDatabaseMissing('posts', ['id' => $this->post->id]);
-        $response->assertOk();
-    }
+  /** @test */
+  public function delete_post()
+  {
+    $response = $this->deleteJson(route('posts.destroy', $this->post->id));
+    $this->assertDatabaseMissing('posts', ['id' => $this->post->id]);
+    $response->assertOk();
+  }
 
-    /** @test */
-    public function fetch_single_post()
-    {
-        $this->getJson(route('posts.show',
-            $this->post->id))->assertOk()->assertJson($this->post->toArray());
-    }
+  /** @test */
+  public function fetch_single_post()
+  {
+    $this->getJson(route('posts.show',
+      $this->post->id))->assertOk()->assertJson($this->post->toArray());
+  }
 
-    /** @test */
-    public function fetch_all_posts()
-    {
-      $this->_createPost(['media_id' => $this->media->id]);
+  /** @test */
+  public function fetch_single_post_with_displays_ids()
+  {
+    $this->withoutExceptionHandling();
+    $displays = Display::factory(5)->create();
+    $displays_ids = $displays->pluck(['id'])->toArray();
+    $this->post->displays()->attach($displays_ids);
+    $this->getJson(route('posts.show', ['post' => $this->post->id, 'withDisplaysIds' => true],
+    ))->assertOk()->assertJson([...$this->post->toArray(), 'displays_ids' => $displays_ids]);
+  }
 
-      $this->getJson(route('posts.index'))->assertOk()->assertJsonCount(2,
-        'data')->assertJsonFragment($this->post->toArray());
-    }
+  /** @test */
+  public function fetch_all_posts()
+  {
+    $this->_createPost(['media_id' => $this->media->id]);
 
-    /** @test */
-    public function ensure_only_description_is_updated_even_if_more_fields_are_sent(
-    )
-    {
-        $old_media_id = $this->post->media->id;
-        $new_values = [
-            'description'   => Str::random(20), 'media_id' => 2,
-            'recurrence_id' => 2
-        ];
+    $this->getJson(route('posts.index'))->assertOk()->assertJsonCount(2,
+      'data')->assertJsonFragment($this->post->toArray());
+  }
 
-        $this->putJson(route('posts.update', $this->post->id),
-            $new_values)->assertOk()->assertJson([
-            'description' => $new_values['description'],
-            'media_id'    => $old_media_id
-        ]);
-    }
+  /** @test */
+  public function ensure_only_description_is_updated_even_if_more_fields_are_sent()
+  {
+    $old_media_id = $this->post->media->id;
+    $new_values = [
+      'description' => Str::random(20), 'media_id' => 2,
+      'recurrence_id' => 2
+    ];
+
+    $this->putJson(route('posts.update', $this->post->id),
+      $new_values)->assertOk()->assertJson([
+      'description' => $new_values['description'],
+      'media_id' => $old_media_id
+    ]);
+  }
 }
