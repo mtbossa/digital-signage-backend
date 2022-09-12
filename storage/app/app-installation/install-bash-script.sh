b6355607b443b064e774acb5c5a942e5a4eaff07
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-09-12 16:22:13 -0300

    Edit install script

--- storage/app/app-installation/install-bash-script.sh
+++ storage/app/app-installation/install-bash-script.sh
@@ -4,17 +4,10 @@ echo ""
 echo "My Command Line Installer"
 echo ""
 
-echo "Updating packages"
-
-apt update
-
-echo "Upgrading packages"
-
-apt upgrade
-
+echo ""
 echo "Creating .env file and folders"
+echo ""
 
-# Create destination folder
 INSTALLATIONFOLDER="$HOME/intus"
 MEDIASFOLDER=${INSTALLATIONFOLDER}/medias
 DATAFOLDER=${INSTALLATIONFOLDER}/data
@@ -36,13 +29,17 @@ LOGS_FOLDER_PATH=${LOGSFOLDER}
 EOF
 )
 
-echo $env_string > ${INSTALLATIONFOLDER}/.env
+echo "$env_string" > ${INSTALLATIONFOLDER}/.env # variable inside "" so new lines are preserved
 
-echo "Downloading docker-compose"
- 
+echo ""
+echo "Downloading docker-compose file"
+echo ""
+
 curl -H GET http://192.168.0.108:80/api/docker/installer/download -o ${INSTALLATIONFOLDER}/docker-compose.yml
 
+echo ""
 echo "Creating docker startup bash script and making it run automatically"
+echo ""
 
 app_startup_script=$(cat << EOF
 #!/bin/bash
@@ -50,26 +47,25 @@ cd $HOME/intus
 docker compose up
 EOF
 )
-echo $app_startup_script > ${INSTALLATIONFOLDER}/intus-startup.sh
+echo "$app_startup_script" > ${INSTALLATIONFOLDER}/intus-startup.sh
 run_app=@bash ${INSTALLATIONFOLDER}/intus-startup.sh
 echo $run_app >> /etc/xdg/lxsession/LXDE-pi/autostart
 
+echo ""
 echo "Making Raspberry automatically open browser on startup"
+echo ""
 
 startup=@chromium-browser --kiosk localhost:45691
 echo $startup >> /etc/xdg/lxsession/LXDE-pi/autostart
 
+echo ""
 echo "Downloading Docker"
+echo ""
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-usermod -aG docker ${USER}
+sudo usermod -aG docker ${USER}
 
 echo ""
-echo "Installation complete.  Rebooting"
+echo "Installation complete.  You must reboot the system"
 echo ""
-
-reboot now
-
-# Exit from the script with success (0)
-exit 0
