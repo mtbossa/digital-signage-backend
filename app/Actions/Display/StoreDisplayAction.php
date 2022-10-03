f932f07fa79a0161f381d96593255ac52c99bd7c
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-10-03 09:58:45 -0300

    Run code style

--- app/Actions/Display/StoreDisplayAction.php
+++ app/Actions/Display/StoreDisplayAction.php
@@ -9,20 +9,20 @@ use App\Models\Store;
 
 class StoreDisplayAction
 {
-    public function handle(StoreDisplayRequest $request): Display
-    {
-        $display = Display::create($request->except(['raspberry_id']));
+  public function handle(StoreDisplayRequest $request): Display
+  {
+    $display = Display::create($request->except(['raspberry_id']));
 
-        if ($request->raspberry_id) {
-            $raspberry = Raspberry::findOrFail($request->raspberry_id);
-          $display->raspberry()->save($raspberry);
-        }
-
-      if ($request->store_id) {
-        $store = Store::findOrFail($request->store_id);
-        $display->store()->associate($store);
-      }
-
-      return $display;
+    if ($request->raspberry_id) {
+      $raspberry = Raspberry::findOrFail($request->raspberry_id);
+      $display->raspberry()->save($raspberry);
     }
+
+    if ($request->store_id) {
+      $store = Store::findOrFail($request->store_id);
+      $display->store()->associate($store);
+    }
+
+    return $display;
+  }
 }
