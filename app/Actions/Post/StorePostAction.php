e3dd544f371fd39b5e45906427243847a1629c22
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2022-12-16 19:50:55 -0300

    Remove unused method

--- app/Actions/Post/StorePostAction.php
+++ app/Actions/Post/StorePostAction.php
@@ -46,13 +46,4 @@ class StorePostAction
 
     return $post;
   }
-
-  private function schedulePostExpiredEvent(Post $post)
-  {
-    $end_date = $post->end_date;
-    $end_time = $post->end_time;
-    $end = Carbon::createFromFormat('Y-m-d H:i:s', "$end_date $end_time");
-    $delay = $end->diffInSeconds(now());
-    ExpirePost::dispatch($post)->delay($delay);
-  }
 }
